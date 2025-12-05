beedd07083be911edf9f3f0e20eb6f17814ab7de
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-12-05 17:58:44 +0700

    Adjust bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,7 @@
 </svelte:head>
 
 <div
-  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen use:addSparticles"
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
 	<div class={"fixed bottom-0 w-full duration-200 flex p-10 z=[10]" +
 		(y > 0 ? 'opacity-full pointer-events-auto':'pointer-events-none opacity-0')
@@ -36,7 +36,7 @@
 	<Header y={y}/>
 	{@render children()}
 	<Footer />
-  <Particles className="absolute inset-0" refresh={true} />
+  <Particles className="absolute inset-0 -z-10" refresh={true} />
 </div>
 
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
